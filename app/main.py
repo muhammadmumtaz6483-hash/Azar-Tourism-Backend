19f1f959176b6e0f84920ba00f6c6b8cf4f94dfd
Malik Moin <147584474+MalikMoin@users.noreply.github.com>
2026-02-19 17:10:54 +0500

    Egypt Table Add In Backend

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from routes.novotel_routes import n_router
 from routes.auth import router as auth_router
 from routes.hotels_info_routes import router as hotels_info_router  
 from routes.turkey_invoices_routes import router as turkey_invoices_router 
+from routes.egypt_invoices_routes import router as egypt_invoices_router    
 from routes.admin_routes import router as admin_router  
 from routes.dashboard_data import router as dashboard_router    
 # from routes.employees_routes import router as employees_router 
@@ -75,6 +76,7 @@ app.include_router(auth_router)
 app.include_router(n_router)
 app.include_router(hotels_info_router)
 app.include_router(turkey_invoices_router)
+app.include_router(egypt_invoices_router)   
 app.include_router(admin_router)
 app.include_router(dashboard_router)
 
