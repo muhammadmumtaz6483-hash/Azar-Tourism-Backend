eca970c8d254dfa0dc83dff4a1bd95af1e05a47a
Malik Moin <147584474+MalikMoin@users.noreply.github.com>
2026-04-10 17:01:46 +0500

    UK Endpoints Update

--- app/routes/dashboard_data.py
+++ app/routes/dashboard_data.py
@@ -9,7 +9,7 @@ from routes.services.invoice_service import get_all_invoice_records
 from routes.services.turkey_hotel_service import get_all_turkey_hotels, get_all_turkey_hotels_by_page
 from routes.services.egypt_hotel_service import get_all_egypt_hotels ,get_all_egypt_hotels_by_page  
 from routes.services.malaysia_hotel_service import get_all_malaysia_hotels, get_all_malaysia_hotels_by_page
-# from routes.services.uk_hotel_service import get_all_uk_hotels, get_all_uk_hotels_by_page
+from routes.services.uk_hotel_service import get_all_uk_hotels, get_all_uk_hotels_by_page
 # from routes.services.dubai_hotel_service import get_all_dubai_hotels, get_all_dubai_hotels_by_page
 
 router = APIRouter(
@@ -23,7 +23,7 @@ async def dashboard_data(db: AsyncSession = Depends(get_db)):
     turkey_hotels_data = await get_all_turkey_hotels(db)
     egypt_hotels_data = await get_all_egypt_hotels(db)  
     malaysia_hotels_data = await get_all_malaysia_hotels(db)    
-    # uk_hotels_data = await get_all_uk_hotels(db)
+    uk_hotels_data = await get_all_uk_hotels(db)
     # dubai_hotels_data = await get_all_dubai_hotels(db)
 
     return {
@@ -34,7 +34,7 @@ async def dashboard_data(db: AsyncSession = Depends(get_db)):
             "turkey_hotels": turkey_hotels_data,
             "egypt_hotels": egypt_hotels_data,
             "malaysia_hotels": malaysia_hotels_data,
-            # "uk_hotels": uk_hotels_data,
+            "uk_hotels": uk_hotels_data,
             # "dubai_hotels": dubai_hotels_data
         }
     }
